076cd0b6e174c0c54e0c1d2d447864371567053e
Brian <diazbrian47@gmail.com>
2023-11-27 21:12:43 -0500

    Creacion del proyecto

---
+++ unamano/unamano/src/main/java/com/unamano/unamano/UnamanoApplication.java
@@ -0,0 +1,13 @@
+package com.unamano.unamano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnamanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnamanoApplication.class, args);
+	}
+
+}
---
+++ unamano/unamano/src/test/java/com/unamano/unamano/UnamanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unamano.unamano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnamanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
