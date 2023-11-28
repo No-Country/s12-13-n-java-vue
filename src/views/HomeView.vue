624a346daa8f7207b3b3e6c28a86c70c613ab62a
Leandro Insfran <leandro.insfran@systelec.com>
2023-11-28 10:46:05 -0300

    my first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import HelloWorld from '../components/HelloWorld.vue'
+
+  //pasar props
+  // defineProps({
+  //   msg: {
+  //     type: String,
+  //     required: true
+  //   }
+
+  // })
+
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
