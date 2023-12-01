a608312266e4155efc2851c4d4ec8f87ac7ab45e
Leandro Insfran <leandro.insfran@systelec.com>
2023-12-01 17:33:08 -0300

    reestructuracion

---
+++ src/components/SectionTutorial.vue
@@ -0,0 +1,53 @@
+<script setup>
+  import { ref } from 'vue'
+</script>
+
+<template>
+
+  <section class="conteiner">
+
+    <!-- TODO CAROUSEL -->
+    <div class="carrousel">
+      <h1>Caroousel</h1>
+    </div>
+
+    <div class="container-description">
+      <h3>CUBRE TUS NECESIDADES</h3>
+      <button class="description-btn">Inicia</button>
+    </div>
+
+  </section>
+
+</template>
+
+<style scoped>
+  .carrousel {
+    margin-top: 10px;
+  }
+  .container-description h3 {
+    color: #fff;
+  }
+  .conteiner,
+  .container-description {
+    display: flex;
+    padding: 0px 32px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-direction: column;
+    background-color: #F40909;
+  }
+
+  .description-btn,
+  .blog-btn {
+    width: 328px;
+    height: 51px;
+    padding: 10px;
+    border-radius: 6px;
+    border: solid 3px #fff;
+    background-color: transparent;
+    color: white;
+    font-size: 20px;
+  }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup>
-  import { ref } from 'vue'
-
-
+  import SectionInfo from '@/components/SectionInfo.vue'
+  import SectionTutorial from '@/components/SectionTutorial.vue'
 
 
   //pasar props
@@ -17,69 +16,14 @@
 
 <template>
   <main>
-
-    <header>ESPACIO PARA EL NAVBAR</header>
-
-    <section id="conocenos" class="conteiner">
-      
-      <img src="@/assets/images/principal.svg" alt="principal">
-      
-      <div class="conteiner-section1">
-        <h3>CONECTAMOS GENERACIONES</h3>
-        <P> Nuestra aplicación te ayuda a crear posiciones de trabajo personalizadas y conectar con personas comprometidas y listas para trabajar</P>
-        <button class="section1-btn"> Ofrece empleo </button>
-        <p class="div">___________________ ó ___________________</p>
-      </div>
-
-      <div class="container-description">
-        <p>Nuestra aplicación te ayuda a crear posiciones de trabajo personalizadas y conectar con personas comprometidas y listas para trabajar.</p>
-        <button style="background-color: #1D3D8F;" class="description-btn">Busca oportunidades</button>
-      </div>
-
-    </section>
-
-    <!-- seccion blog -->
-
+    
+    <SectionInfo />
+    <SectionTutorial />
    
   </main>
 </template>
 
 <style scoped>
-  .conteiner, .conteiner-section1, .container-description, .section-blog {
-    display: flex;
-    padding: 0px 32px;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-    flex-direction: column;
-  }
-  .conteiner-section1 {
-    padding: 10px;
-  }
-
-  .conteiner-section1 h3 {
-    font-size: 17px;
-    font-weight: bold;
-    margin: 0;
-  }
-  .conteiner-section1 p, .container-description p {
-    text-align: center;
-    margin: 0;
-    margin-bottom: 15px;
-  }
-  .section1-btn, .description-btn,.blog-btn {
-    width: 328px;
-    height: 51px;
-    padding: 10px;
-    border-radius: 6px;
-    background-color: #149ED7;
-    color: white;
-    font-size: 20px;
-  }
-  .conteiner-section1 .div {
-    margin-top: 20px;
-  }
-
- /* Seccion Blog */
+ 
  
 </style>
