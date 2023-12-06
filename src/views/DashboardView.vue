108d3dc02830eeb6eed62fc41374f787f32c3b60
Artem <lokomotivspb@gmail.com>
2023-12-06 16:20:29 -0300

    feat: dashboard nav styles

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,20 +4,37 @@ import SectionHeader from '../components/SectionHeader.vue'
 
 <template>
   <main>
-    <SectionHeader/>
+    <SectionHeader />
     <nav class="nav">
       <ul class="nav__list">
-        <li class="nav__item">
+        <li class="nav__item link" @click="toggleNavItem">
           <div class="nav__item-container">
-            <img class="nav__item-image" src="../assets/images/home-icon.svg" />
-            <p class="nav__item-text nav__item-text_home">home</p>
+            <img
+              class="nav__item-image"
+              :class="{ active: isActive }"
+              src="../assets/images/home-icon.svg"
+            />
+            <p class="nav__item-text" :class="{ active: isActive }">Inicio</p>
           </div>
-          <div class="nav__item-underline"></div>
         </li>
-        <li class="nav__item">
+        <li class="nav__item link" @click="toggleNavItem">
           <div class="nav__item-container">
-            <img class="nav__item-image" src="../assets/images/comment-icon.svg" />
-            <p class="nav__item-text">chat</p>
+            <img
+              class="nav__item-image"
+              :class="{ active: !isActive }"
+              src="../assets/images/comment-icon.svg"
+            />
+            <p class="nav__item-text" :class="{ active: !isActive }">Chats</p>
+          </div>
+        </li>
+        <li class="nav__item link" @click="toggleNavItem">
+          <div class="nav__item-container">
+            <img
+              class="nav__item-image"
+              :class="{ active: !isActive }"
+              src="../assets/images/history-icon.svg"
+            />
+            <p class="nav__item-text" :class="{ active: !isActive }">Historial</p>
           </div>
         </li>
       </ul>
@@ -53,6 +70,21 @@ import SectionHeader from '../components/SectionHeader.vue'
   </main>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      activeItems: [false, false, false]
+    }
+  },
+  methods: {
+    toggleNavItem(index) {
+      this.$set(this.activeItems, index, !this.activeItems[index])
+    }
+  }
+}
+</script>
+
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Yaldevi:wght@600&display=swap');
 ul,
@@ -62,9 +94,16 @@ li {
   padding: 0;
   list-style: none;
 }
+
+.link:hover {
+  cursor: pointer;
+}
+
+.active {
+  color: #149ed7;
+}
 .nav {
-  padding: 15px 42px;
-  height: 42px;
+  padding: 8px 16px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -72,7 +111,7 @@ li {
 .nav__list {
   display: flex;
   justify-content: center;
-  gap: 70px;
+  gap: 16px;
 }
 .nav__item {
   display: flex;
@@ -80,31 +119,30 @@ li {
   gap: 10px;
 }
 .nav__item-image {
-  width: 30px;
-  height: 27.704px;
+  width: 35px;
+  height: 31px;
+  color: #3960c2;
 }
 
 .nav__item-container {
   display: flex;
   align-items: baseline;
+  flex-direction: column;
+  align-items: center;
   gap: 13px;
+  width: 120.333px;
+  color: #3960c2;
 }
 
 .nav__item-text {
-  color: var(--blue2, #149ed7);
   font-family: 'Yaldevi';
   font-size: 27.683px;
   font-style: normal;
   font-weight: 600;
-  line-height: 67%; /* 18.548px */
+  line-height: 67%;
   letter-spacing: 2.215px;
 }
 .nav__item-text_home {
-  color: var(--blue1, #1d3d8f);
-}
-.nav__item-underline {
-  border-bottom: solid 9px #1d3d8f;
-  width: 130px;
-  border-radius: 20px;
+  color: var(--blue1, #3960c2);
 }
 </style>
