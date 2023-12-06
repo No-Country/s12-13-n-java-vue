a121e4fde7acef528075b0f09157a6d0893970a2
David <figuererodavid@gmail.com>
2023-12-05 22:38:49 -0300

    intermediate table between worker and occupation

---
+++ server/unamano/src/main/java/com/latam/unamano/persistence/entities/workerEntity/Worker.java
@@ -0,0 +1,80 @@
+package com.latam.unamano.persistence.entities.workerEntity;
+
+import com.latam.unamano.persistence.entities.addressEntity.Address;
+import com.latam.unamano.persistence.entities.ocupationEntity.Occupation;
+import com.latam.unamano.persistence.entities.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker extends User {
+
+    @ManyToMany
+    @JoinTable(name = "worker_occupations",
+            joinColumns = @JoinColumn(name = "worker_id"),
+            inverseJoinColumns = @JoinColumn(name = "occupation_id"))
+    private List<Occupation> occupations;
+
+    public static final class WorkerBuilder{
+        private String firstname;
+        private String lastname;
+        private String email;
+        private Address address;
+        private String phone;
+        private List<Occupation> occupations;
+
+        public WorkerBuilder(){}
+
+        public WorkerBuilder firstname(String firstname){
+            this.firstname = firstname;
+            return this;
+        }
+
+
+        public WorkerBuilder lastname(String lastname){
+            this.lastname = lastname;
+            return this;
+        }
+
+        public WorkerBuilder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public WorkerBuilder address(Address address){
+            this.address = address;
+            return this;
+        }
+
+        public WorkerBuilder occupation(List<Occupation> occupationList){
+            this.occupations = occupationList;
+            return this;
+        }
+
+        public WorkerBuilder phone(String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public Worker build(){
+            Worker worker = new Worker();
+            worker.setFirstName(firstname);
+            worker.setLastName(lastname);
+            worker.setEmail(email);
+            worker.setAddresses(address);
+            worker.setOccupations(occupations);
+            worker.setPhoneNumber(phone);
+            return worker;
+        }
+    }
+}
