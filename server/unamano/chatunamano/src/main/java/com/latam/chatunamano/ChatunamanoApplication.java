88eec4953601e5e51ba759093df3ef7e04454289
Brian <diazbrian47@gmail.com>
2023-12-13 18:53:35 -0500

    Creando chatService

---
+++ server/unamano/chatunamano/src/main/java/com/latam/chatunamano/ChatunamanoApplication.java
@@ -0,0 +1,13 @@
+package com.latam.chatunamano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatunamanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatunamanoApplication.class, args);
+	}
+
+}
---
+++ server/unamano/chatunamano/src/test/java/com/latam/chatunamano/ChatunamanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latam.chatunamano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatunamanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
