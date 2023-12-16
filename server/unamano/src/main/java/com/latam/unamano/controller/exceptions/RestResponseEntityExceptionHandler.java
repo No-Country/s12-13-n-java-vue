7e733737e7dd5591fde4fbe505e00da8938cc680
Enzo <91644783+aesaries@users.noreply.github.com>
2023-12-16 00:15:03 -0300

    Emparejando el HandlerException

--- server/unamano/src/main/java/com/latam/unamano/controller/exceptions/RestResponseEntityExceptionHandler.java
+++ server/unamano/src/main/java/com/latam/unamano/controller/exceptions/RestResponseEntityExceptionHandler.java
@@ -1,15 +1,10 @@
 package com.latam.unamano.controller.exceptions;
 
-
-import com.latam.unamano.exceptions.RatingNotFoundException;
-import com.latam.unamano.exceptions.UserNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import com.latam.unamano.exceptions.BadDataEntryException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,6 +13,8 @@ import org.springframework.web.context.request.WebRequest;
 import com.latam.unamano.dto.exceptions.ErrorResponse;
 import com.latam.unamano.exceptions.IncorretRoleException;
 import com.latam.unamano.exceptions.UsernameOrPasswordIncorretException;
+
+import org.springframework.security.access.AccessDeniedException;
 
 @RestControllerAdvice
 public class RestResponseEntityExceptionHandler {
@@ -37,7 +34,6 @@ public class RestResponseEntityExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse("Username or password", ex.getMessage()),
                 HttpStatus.BAD_REQUEST);
     }
-
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> err404(EntityNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
@@ -51,19 +47,8 @@ public class RestResponseEntityExceptionHandler {
     public ResponseEntity<String> badDataEntry(HttpMessageNotReadableException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getLocalizedMessage());
     }
-
-
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Object> handlerResourceNotFoundException(UserNotFoundException ex, WebRequest request) {
-        return new ResponseEntity<>(new ErrorResponse("User", ex.getMessage()),
-                HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> accessDenied(AccessDeniedException e){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
     }
-
-    @ExceptionHandler(RatingNotFoundException.class)
-    public ResponseEntity<Object> handlerResourceNotFoundException(RatingNotFoundException ex, WebRequest request) {
-        return new ResponseEntity<>(new ErrorResponse("Rating", ex.getMessage()),
-                HttpStatus.BAD_REQUEST);
-    }
-
 }
