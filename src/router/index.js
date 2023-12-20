ad6b7aea6b1f4c178d0978676a646c1dbd62bc9b
Nazareno D Susunday <nazarenosusunday@gmail.com>
2023-12-20 20:44:06 -0300

    solucionado index

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import useAuth from '@/stores/auth'
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-import PostulationsView from '../views/PostulationsView.vue'
-import ChatView from '../views/ChatView.vue'
-import DashboardView from '../views/DashboardView.vue'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import DashboardTrabajador from '../views/DashboardTrabajador.vue'
-import ChatView from '../views/ChatView.vue'
+import PostulationsView from '@/views/PostulationsView.vue'
+import ChatView from '@/views/ChatView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import HomeView from '@/views/HomeView.vue'
+import DashboardWorker from '@/views/DashboardWorker.vue'
+import HistoryPage from '@/views/HistoryPage.vue'
+
 
 const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
   {
     path: '/login',
     name: 'login',
@@ -24,35 +29,45 @@ const routes = [
     name: 'dashboard',
     component: DashboardView,
     meta: {
-
       requireAuth: true,
     },
-
-    {
-      path: '/DashboardTrabajador',
-      name: 'DashboardTrabajador',
-      component: DashboardTrabajador,
-      meta: {
-        requereAuth: true
-      },
+  },
+  {
+    path: '/worker',
+    name: 'worker',
+    component: DashboardWorker,
+    meta: {
+      requireAuth: true,
     },
-
-    {
-      path: '/Chat',
-      name: 'Chat',
-      component: ChatView,
-      meta: {
-        requereAuth: true
-      },
+  },
+  {
+    path: '/posts',
+    name: 'postulations',
+    component: PostulationsView,
+    query: {
+      category: ''
     },
+    meta: {
+      requireAuth: true,
+    }
+  },
+
+  {
+    path: '/Chat',
+    name: 'Chat',
+    component: ChatView,
+    meta: {
+      requireAuth: true,
+    }
+  },
 
   {
     path: '/history',
     name: 'history',
     component: HistoryPage,
-   /* meta: {
+    meta: {
       requireAuth: true,
-    }*/
+    }
   },
 ]
 
